d99df8ac418acd183a2e8edfd09cfda9bf9fedff
benjlebe741 <benjlebe741@ed.amdsb.ca>
2023-04-22 12:51:49 -0400

    Add project files.

---
+++ pong/Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace pong
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.gameTimer = new System.Windows.Forms.Timer(this.components);
+            this.player2ScoreLabel = new System.Windows.Forms.Label();
+            this.player1ScoreLabel = new System.Windows.Forms.Label();
+            this.winLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // gameTimer
+            // 
+            this.gameTimer.Enabled = true;
+            this.gameTimer.Interval = 15;
+            this.gameTimer.Tick += new System.EventHandler(this.gameTimer_Tick);
+            // 
+            // player2ScoreLabel
+            // 
+            this.player2ScoreLabel.BackColor = System.Drawing.Color.Transparent;
+            this.player2ScoreLabel.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.player2ScoreLabel.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.player2ScoreLabel.Location = new System.Drawing.Point(599, 9);
+            this.player2ScoreLabel.Name = "player2ScoreLabel";
+            this.player2ScoreLabel.Size = new System.Drawing.Size(127, 59);
+            this.player2ScoreLabel.TabIndex = 0;
+            this.player2ScoreLabel.Text = "PLAYER 2:   0";
+            this.player2ScoreLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // player1ScoreLabel
+            // 
+            this.player1ScoreLabel.BackColor = System.Drawing.Color.Transparent;
+            this.player1ScoreLabel.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.player1ScoreLabel.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.player1ScoreLabel.Location = new System.Drawing.Point(466, 9);
+            this.player1ScoreLabel.Name = "player1ScoreLabel";
+            this.player1ScoreLabel.Size = new System.Drawing.Size(127, 59);
+            this.player1ScoreLabel.TabIndex = 1;
+            this.player1ScoreLabel.Text = "PLAYER 1:   0";
+            this.player1ScoreLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // winLabel
+            // 
+            this.winLabel.BackColor = System.Drawing.Color.Transparent;
+            this.winLabel.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.winLabel.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.winLabel.Location = new System.Drawing.Point(211, 191);
+            this.winLabel.Name = "winLabel";
+            this.winLabel.Size = new System.Drawing.Size(303, 112);
+            this.winLabel.TabIndex = 2;
+            this.winLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(738, 470);
+            this.Controls.Add(this.winLabel);
+            this.Controls.Add(this.player1ScoreLabel);
+            this.Controls.Add(this.player2ScoreLabel);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer gameTimer;
+        private System.Windows.Forms.Label player2ScoreLabel;
+        private System.Windows.Forms.Label player1ScoreLabel;
+        private System.Windows.Forms.Label winLabel;
+    }
+}
+
---
+++ pong/Form1.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Management.Instrumentation;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace pong
+{
+    public partial class Form1 : Form
+    {
+        //KEYDOWN/KEYUP LISTS:
+        bool[] WSADUpDownLeftRight = new bool[] { false, false, false, false, false, false, false, false };
+        Keys[] keysToCheck = new Keys[] { Keys.W, Keys.S, Keys.A, Keys.D, Keys.Up, Keys.Down, Keys.Left, Keys.Right };
+
+        int[][] determineDirectionsList = new int[][] { };
+
+        //BRUSHES & PENS
+        new SolidBrush blueBrush = new SolidBrush(Color.LightBlue);
+        new SolidBrush whiteBrush = new SolidBrush(Color.White);
+        new Pen whitePen = new Pen(Color.White, 5);
+
+
+        //GAME OBJECTS
+        Rectangle[] gameObjects = new Rectangle[] //0 = ball, 1 = player1, 2 = player2
+        {
+        new Rectangle(480, 240, 20, 20),
+        new Rectangle(0, 320, 20, 80),
+        new Rectangle(0, 80, 20, 80),
+        };
+
+        Point[] locationList = new Point[] { 
+            new Point(480, 240),
+            new Point(0, 320),  
+            new Point(0, 80),
+        };
+
+        //TRACKING OBJECT INFORMATION: 0 = BALL 1 = PLAYER1 2 = PLAYER2
+        int[] objectDirectionsY = new int[] { -1, 0, 0 };
+        int[] objectDirectionsX = new int[] { -1, 0, 0 };
+        int[] objectSpeeds = new int[] { 3, 3, 3 };
+        int[] playerScores = new int[] { 0, 0 };
+        int currentPlayer = 2;
+        public Form1()
+        {
+            InitializeComponent();
+            determineDirectionsList = new int[][]
+          {
+      new int[]{  0,1,objectDirectionsY[1]},
+      new int[]{  2,3,objectDirectionsX[1]},
+      new int[]{  4,5,objectDirectionsY[2]},
+      new int[]{  6,7,objectDirectionsX[2]},
+          };
+        }
+
+        private void gameTimer_Tick(object sender, EventArgs e)
+        {
+            for (int i = 0; i < 2; i++) {
+                if (playerScores[i] > 2) 
+                {
+                    winLabel.Text = $"PLAYER {i + 1} IS THE VICTOR!";
+                    gameTimer.Enabled = false; 
+                }
+            }
+
+
+            //Determening all player directions:
+            for (int i = 0; i < determineDirectionsList.Length; i++)
+            {
+                if (WSADUpDownLeftRight[determineDirectionsList[i][0]] == true && WSADUpDownLeftRight[determineDirectionsList[i][1]] == false) { determineDirectionsList[i][2] = -1; }
+                else if (WSADUpDownLeftRight[determineDirectionsList[i][1]] == true && WSADUpDownLeftRight[determineDirectionsList[i][0]] == false) { determineDirectionsList[i][2] = 1; }
+                else { determineDirectionsList[i][2] = 0; }
+            }
+
+            objectDirectionsY[1] = determineDirectionsList[0][2];
+            objectDirectionsX[1] = determineDirectionsList[1][2];
+            objectDirectionsY[2] = determineDirectionsList[2][2];
+            objectDirectionsX[2] = determineDirectionsList[3][2];
+
+            for (int i = 0; i <= 2; i++)
+            {
+                //IF ANY OBJECTS ARE HITTING THE WALLS; PUSH THEM BACK
+                //TOP WALL
+                if (gameObjects[i].Y <= 0)
+                {
+                    if (i == 0) { objectDirectionsY[0] *= -1; }
+                    gameObjects[i].Y = 0 + 1;
+                }
+                //BOTTOM WALL
+                if (gameObjects[i].Y >= this.Height - gameObjects[i].Height)
+                {
+                    if (i == 0) { objectDirectionsY[0] *= -1; }
+                    gameObjects[i].Y = this.Height - gameObjects[i].Height - 1;
+                }
+                //LEFT WALL
+                if (gameObjects[i].X < 0)
+                {
+                    if (i == 0)
+                    {
+                        updateScore(currentPlayer - 1);
+                        //increase the speed of the ball
+                        objectSpeeds[0] = 3;
+
+                        for (int j = 0; j < 3; j++) { gameObjects[j].Location = locationList[j]; }
+                    }
+                    else
+                    {
+                        gameObjects[i].X = 0 + 1;
+                    }
+                }
+                //RIGHT WALL
+                if (gameObjects[i].X > this.Width - gameObjects[0].Width)
+                {
+                    if (i == 0) { objectDirectionsX[0] *= -1; }
+                    gameObjects[i].X = this.Width - gameObjects[0].Width - 1;
+                }
+
+                //OBJECTS INTERACTING WITH EACHOTHER
+                if (gameObjects[0].IntersectsWith(gameObjects[i]) && i == currentPlayer && objectDirectionsX[0] == -1)
+                {
+                    objectDirectionsX[0] *= -1;
+                    gameObjects[0].X = gameObjects[i].X + gameObjects[i].Width;
+                    objectSpeeds[0]++;
+                    if (i == 1) { currentPlayer = 2; }
+                    else { currentPlayer = 1; }
+                }
+                //UPDATE OBJECT POSITIONS
+                gameObjects[i].X += objectDirectionsX[i] * objectSpeeds[i];
+                gameObjects[i].Y += objectDirectionsY[i] * objectSpeeds[i];
+            }
+
+
+
+            Refresh();
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            e.Graphics.FillRectangle(whiteBrush, gameObjects[0]);
+            e.Graphics.FillRectangle(blueBrush, gameObjects[1]);
+            e.Graphics.FillRectangle(blueBrush, gameObjects[2]);
+            e.Graphics.DrawRectangle(whitePen, gameObjects[currentPlayer]);
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            checkKey(true, e);
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            checkKey(false, e);
+        }
+
+        void updateScore(int player)
+        {
+            playerScores[player]++;
+            player1ScoreLabel.Text = $"PLAYER 1:  {playerScores[1]}";
+            player2ScoreLabel.Text = $"PLAYER 2:  {playerScores[0]}";
+        }
+
+        void checkKey(bool trueOrFalse, KeyEventArgs e)
+        {
+            for (int i = 0; i < keysToCheck.Length; i++)
+            {
+                if (e.KeyCode == keysToCheck[i])
+                {
+                    WSADUpDownLeftRight[i] = trueOrFalse;
+                }
+            }
+        }
+    }
+}
